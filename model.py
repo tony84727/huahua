6c33cad6c81d135e0d850864b52b52ad157d8d3d
Tony Duan <tony84727@gmail.com>
2021-08-14 11:41:46 +0800

    fix: add description to server model

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column
+from sqlalchemy.sql.expression import null
 from sqlalchemy.sql.sqltypes import Integer, VARCHAR
 from sqlalchemy.orm import registry
 
@@ -12,7 +13,8 @@ class Server(Base):
 
     id = Column(Integer, primary_key=True)
     address = Column(VARCHAR(512), index=True)
-    alias = Column(VARCHAR(256), nullable=True, index=True)
+    alias = Column(VARCHAR(256), nullable=True, unique=True)
+    description = Column(VARCHAR(1024), nullable=True)
 
     def __repr__(self) -> str:
-        return f"Server(id={self.id!r}, address={self.address!r}, alias={self.alias!r})"
+        return f"Server(id={self.id!r}, address={self.address!r}, alias={self.alias!r}, description{self.description!r})"

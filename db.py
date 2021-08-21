c93c971dbbd62ee5a469c3b95fdc8d3550d7aae3
Tony Duan <tony84727@gmail.com>
2021-08-22 02:49:32 +0800

    feat: make database configurable (#6)
    also, install alembic into the docker image

--- db.py
+++ db.py
@@ -1,6 +1,17 @@
-from sqlalchemy import create_engine
+import os
 
 
-def new_engine(*, user='root', host='localhost', name='huahua'):
-    dsn = f'mysql+pymysql://{user}@{host}/{name}'
-    return create_engine(dsn, echo=True, future=True)
+def get_dsn(*,
+            host=os.environ.get('DB_HOST', 'localhost'),
+            user=os.environ.get('DB_USER', 'root'),
+            password=os.environ.get('DB_PASSWORD', ''),
+            name=os.environ.get('DB_NAME', 'huahua'),
+            ) -> str:
+    """
+        Return database connection string.
+        The connection parameters are figured out by the following order:
+        1. Parameters written directly in code
+        2. Environment variables: DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+        3. Hardcoded default
+    """
+    return f'mysql+pymysql://{user}:{password}@{host}/{name}'
